2642033c72e4b184a78311ed8627a56f60f4c39d
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-04-01 03:04:34 +0900

    Add type ekikan_t

--- 08-record.ml
+++ 08-record.ml
@@ -6,6 +6,7 @@ type ekimei_t = {
   shozoku : string; (* 所属する路線名 *)
 }
 
+
 (* 目的：駅名のデータ ekimei_t を受け取り「路線名，駅名（かな）」の形の文字列を返す *)
 (* hyoji : ekimei_t -> string *)
 let hyoji ekimei = match ekimei with
@@ -14,3 +15,13 @@ let hyoji ekimei = match ekimei with
 
 (* テスト *)
 let test1 = hyoji {kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku="丸ノ内線"} = "丸ノ内線，茗荷谷（みょうがだに）"
+
+
+(* 駅間（起点の駅名、終点の駅名、経由する路線名、距離、所要時間）を表す型 *)
+type ekikan_t = {
+  kiten : string;  (* 起点の駅名 *)
+  shuten : string; (* 終点の駅名 *)
+  keiyu : string;  (* 経由する路線名 *)
+  kyori : float;   (* 二駅間の距離。km *)
+  jikan : int;     (* 所要時間。分 *)
+}
