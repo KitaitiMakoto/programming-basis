697c5c0ca2538c1c0f907869fc725e7715f6510b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-04-01 02:55:39 +0900

    Implement function

--- 08-record.ml
+++ 08-record.ml
@@ -9,7 +9,8 @@ type ekimei_t = {
 (* 目的：駅名のデータ ekimei_t を受け取り「路線名，駅名（かな）」の形の文字列を返す *)
 (* hyoji : ekimei_t -> string *)
 let hyoji ekimei = match ekimei with
-  {kanji = kj; kana = kn; romaji = r; shozoku = s} -> ""
+  {kanji = kj; kana = kn; romaji = r; shozoku = s} ->
+    s ^ "，" ^ kj ^ "（" ^ kn ^ "）"
 
 (* テスト *)
 let test1 = hyoji {kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku="丸ノ内線"} = "丸ノ内線，茗荷谷（みょうがだに）"
